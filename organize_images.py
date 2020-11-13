c1977c7ae4814b6ddc7ce4eff1fc769c273bc2cd
SKazemii <Kazemi_Saeed@Hotmail.com>
2020-11-13 11:33:19 -0400

    orginizing images on result folder based on their classes

--- organize_images.py
+++ organize_images.py
@@ -2,49 +2,56 @@
 import os
 import glob
 import datetime
+import pandas as pd
 
-# print start time
-print ("[INFO] program started on - " + str(datetime.datetime.now))
 
 # get the input and output path
-input_path  = "G:\\workspace\\machine-intelligence\\deep-learning\\flower-recognition\\17flowers\\jpg"
-output_path = "G:\\workspace\\machine-intelligence\\deep-learning\\flower-recognition\\dataset\\train"
+input_path = "/Users/saeedkazemi/Documents/Python/footprints-recognition-master/imgs"
+output_path = (
+    "/Users/saeedkazemi/Documents/Python/footprints-recognition-master/results"
+)
+
+csv_path = input_path + "/0output.csv"
+print(csv_path)
+csv_df = pd.read_csv(csv_path)
+csv_df["fileName"] = list(range(1, 2659))
+
+temp = list(range(1, 2659))
+csv_df["fileName"] = [input_path + "/" + str(i) + ".jpeg" for i in temp]
+
+# print the head of csv_df
+print(csv_df.head())
+
+
+# footprints class names
+class_names = csv_df["subjects"].unique()
+print("name of subjects = {}".format(class_names))
+
 
 # get the class label limit
-class_limit = 17
-
-# take all the images from the dataset
-image_paths = glob.glob(input_path + "\\*.jpg")
-
-# variables to keep track
-label = 0
-i = 0
-j = 80
-
-# flower17 class names
-class_names = ["daffodil", "snowdrop", "lilyvalley", "bluebell", "crocus",
-			   "iris", "tigerlily", "tulip", "fritillary", "sunflower", 
-			   "daisy", "coltsfoot", "dandelion", "cowslip", "buttercup",
-			   "windflower", "pansy"]
+class_limit = class_names.__len__()
+print("Number of subjects = {}".format(class_limit))
 
 # change the current working directory
 os.chdir(output_path)
 
 # loop over the class labels
-for x in range(1, class_limit+1):
-	# create a folder for that class
-	os.system("mkdir " + class_names[label])
-	# get the current path
-	cur_path = output_path + "\\" + class_names[label] + "\\"
-	# loop over the images in the dataset
-	for image_path in image_paths[i:j]:
-		original_path = image_path
-		image_path = image_path.split("\\")
-		image_path = image_path[len(image_path)-1]
-		os.system("copy " + original_path + " " + cur_path + image_path)
-	i += 80
-	j += 80
-	label += 1
+for x in range(1, class_limit):
+    # create a folder for each subjects
+    os.system("mkdir " + str(class_names[x]))
 
-# print end time
-print ("[INFO] program ended on - " + str(datetime.datetime.now))
+
+# loop over the images in the imgs folder
+for ind in csv_df.index:
+    original_path = csv_df["fileName"][ind]
+    image = original_path.split("/")
+    image = image[len(image) - 1]
+    cur_path = (
+        output_path
+        + "/"
+        + str(csv_df["subjects"][ind])
+        + "/"
+        + str(csv_df["group"][ind])
+        + "-"
+    )
+    os.system("cp " + original_path + " " + cur_path + image)
